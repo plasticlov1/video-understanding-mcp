670475a9ef88ee1622fea89e71b27589d90a84d1
love f <maphutimothemane@gmail.com>
2026-09-19 19:15:25 +0800

    update new file

--- server.py
+++ server.py
@@ -46,4 +46,4 @@ def watch_video(url: str) ->list:
 if __name__ =="__main__":
     import uvicorn
     app = mcp.sse_app()
-    uvicorn.run(app,host="0.0.0.0",port=8000,proxy_headers=True,forwarded_allow_ips="*")
+    uvicorn.run(app,host="0.0.0.0", port=8000, proxy_headers=True, forwarded_allow_ips="*")
