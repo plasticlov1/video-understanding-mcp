530166b19b347e8a04f5ef20a31573d84b07d33d
love f <maphutimothemane@gmail.com>
2026-09-20 16:07:36 +0800

    update new file

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ import base64
 import numpy as np
 import requests
 import tempfile
-from mcp.server.fastmcp import FastMCP
+from mcp.server.fastmcp import FastMCP,Image
 
 mcp = FastMCP("video-watcher",host="0.0.0.0",port=8000)
 
@@ -12,7 +12,7 @@ def frame_diff(frame1,frame2):
             return np.mean(diff)
 
 @mcp.tool()
-def watch_video(url: str) -> str:  
+def watch_video(url: str):  
     #从URL下载视频到临时文件
     r = requests.get(url)
     tmp = tempfile.NamedTemporaryFile(suffix=".mp4",delete=False)
@@ -25,7 +25,7 @@ def watch_video(url: str) -> str:
 
     frame_count = 0
     prev_frame = None
-    frame_b64 =[]
+    frame_images =[]
 
 
     while True:
@@ -36,12 +36,13 @@ def watch_video(url: str) -> str:
             diff = frame_diff(prev_frame,frame)
             if diff >= 30 or frame_count % (fps * 3)==0:
                 _,buf = cv2.imencode(".jpg",frame)
-                b64 = base64.b64encode(buf).decode("utf-8")
-                frame_b64.append(b64)
+	        if len(frame_images) <15 :
+			frame_images.append(Image(data=buf.tobytes(), format="jpeg"))
+                
         prev_frame = frame
         frame_count+=1
     video.release()
-    return str(frame_b64)
+    return frame_images
 
 if __name__ =="__main__":
     import uvicorn
